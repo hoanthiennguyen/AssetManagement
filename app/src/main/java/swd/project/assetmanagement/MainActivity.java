fa7b361412fa34f1eb5c05b9f627062648ae9d0e
hoanthien <hoanthien97@gmail.com>
2019-11-26 15:12:17 +0700

    after merging

--- app/src/main/java/swd/project/assetmanagement/MainActivity.java
+++ app/src/main/java/swd/project/assetmanagement/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
+import in.mrasif.libs.easyqr.EasyQR;
+import in.mrasif.libs.easyqr.QRScanner;
 import swd.project.assetmanagement.adapter.ViewPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,8 +32,25 @@ public class MainActivity extends AppCompatActivity {
         tabLayoutMainMenu.setupWithViewPager(viewPagerMainContent);
 
     }
-    public void onClickScanQR(View view){
+
+    public void onClickScanQR(View view) {
+//        Intent intent = new Intent(this, QRScanner.class);
+//        startActivityForResult(intent, EasyQR.QR_SCANNER_REQUEST);
         Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
         startActivity(intent);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == EasyQR.QR_SCANNER_REQUEST) {
+
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra(EasyQR.DATA);
+                Toast.makeText(this, "text: " +contents, Toast.LENGTH_SHORT).show();
+            }
+            if(resultCode == RESULT_CANCELED){
+                //handle cancel
+            }
+        }
     }
 }
--- app/src/main/java/swd/project/assetmanagement/repository/AssetService.java
+++ app/src/main/java/swd/project/assetmanagement/repository/AssetService.java
@@ -1,8 +1,5 @@
 package swd.project.assetmanagement.repository;
 
-import java.util.List;
-
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.PUT;
